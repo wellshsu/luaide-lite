14e06326a5dffa7662a73b1a89167a70b8ea16e4
MonkeyKingMKY <j2493835@aliyun.com>
2019-02-12 18:31:27 +0800

    添加 lua 文件的正则排除方法

--- src/context/ExMgr.ts
+++ src/context/ExMgr.ts
@@ -1,257 +1,285 @@
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const fs = require("fs");
-const vscode = require("vscode");
-const UpperLower_1 = require("../formater/UpperLower");
-const FileFormat_1 = require("../formater/FileFormat");
-const PvdTemplate_1 = require("../provider/PvdTemplate");
-const PvdLuats_1 = require("../provider/PvdLuats");
-const Helper_1 = require("../context/Helper");
-const HighlightG_1 = require("../formater/HighlightG");
-class EXMgr {
-    static showAbout(e) {
-        vscode.commands.executeCommand("workbench.extensions.action.showExtensionsWithId", EXMgr.extensionID.toLowerCase());
+
+import * as path from 'path'
+import * as fs from 'fs'
+import { ExtensionContext } from 'vscode'
+import vscode = require('vscode')
+import { UpperLower } from '../formater/UpperLower'
+import { FileFormat } from "../formater/FileFormat"
+import { PvdTemplate } from '../provider/PvdTemplate'
+import { PvdLuats } from '../provider/PvdLuats';
+import { Helper } from "../context/Helper"
+import { HighlightG } from '../formater/HighlightG';
+
+export class EXMgr {
+    public static extensionID = "wellshsu.luaide-lite"
+    public static extensionName = "luaide-lite"
+    public static slogan = "Lite and free, but more professional."
+    public static LUA_MODE: vscode.DocumentFilter = { language: "lua", scheme: "file" }
+    public static LANGUAGE_ID = "lua";
+
+    public static luaOperatorCheck: boolean
+    public static luaFunArgCheck: boolean
+    public static extensionPath: string
+    public static maxFileSize: number = 2048
+    public static changeTextCheck: boolean = true
+    public static moduleFunNestingCheck: boolean
+    public static requireFunNames: Array<string>
+    public static scriptRoots: Array<string>
+    public static isInit: boolean = false
+    public static enableHighlight: boolean = true
+    public static normalHighlightColor: string = "#2B91AF"
+    public static darkHighlightColor: string = "#00D6AA"
+    public static enableFormat: boolean = true
+    public static templateDir: string
+    public static templateDefine: Map<string, string>
+    public static excludes: Array<string>
+    public static excludesRegex: Array<string>
+    public static formatHex: boolean
+    public static enableDiagnostic: boolean
+    public static typescriptDefine: Map<string, string>
+    public static core: string = "emmy"
+    public static javahome: string
+    public static lightParameter: string = "#565656"
+    public static lightGlobal: string = "#2B91AF"
+    public static lightAnnotation: string = "#2B91AF"
+    public static darkParameter: string = "#FFFFFF"
+    public static darkGlobal: string = "#00D6AA"
+    public static darkAnnotation: string = "#00D6AA"
+
+    public static isLegacy: boolean = false
+    public static focused: boolean
+    public static Bar: vscode.StatusBarItem;
+    public static Commands = [
+        { label: "luaide-lite.toUpperCase", desc: 'Shift chars to uppercase', func: UpperLower.toUpperCase },
+        { label: "luaide-lite.toLowerCase", desc: 'Shift carrs to lowercase', func: UpperLower.toLowerCase },
+        { label: "luaide-lite.formatFiles", desc: 'Format file(s)', func: FileFormat.process },
+        { label: "luaide-lite.createTemplate", desc: 'Create template', func: PvdTemplate.process },
+        { label: "luaide-lite.convertTsFile", desc: 'Translate lua to typescript', func: PvdLuats.processFile },
+        { label: "luaide-lite.convertTsText", desc: 'Convert lua to typescript', func: PvdLuats.processText },
+        { label: "luaide-lite.openRes", desc: 'Open res folder', func: EXMgr.openRes },
+        { label: "luaide-lite.about", desc: 'About luaide-lite', func: EXMgr.showAbout },
+    ];
+
+    public static showAbout(e) {
+        vscode.commands.executeCommand("workbench.extensions.action.showExtensionsWithId", EXMgr.extensionID.toLowerCase())
         // vscode.window.showInformationMessage(EXMgr.slogan)
     }
-    static openRes(e) {
-        var resDir = path.join(EXMgr.extensionPath, "res");
-        Helper_1.Helper.ShowInExplorer(resDir);
+
+    public static openRes(e) {
+        var resDir = path.join(EXMgr.extensionPath, "res")
+        Helper.ShowInExplorer(resDir)
     }
-    static initialize(context) {
-        EXMgr.focused = true;
+
+    public static initialize(context: ExtensionContext) {
+        EXMgr.focused = true
         for (var i = 0; i < EXMgr.Commands.length; i++) {
-            var define = EXMgr.Commands[i];
-            vscode.commands.registerCommand(define.label, define.func);
+            var define = EXMgr.Commands[i]
+            vscode.commands.registerCommand(define.label, define.func)
         }
-        EXMgr.Bar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+        EXMgr.Bar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
         context.subscriptions.push(this.Bar);
-        EXMgr.Bar.tooltip = EXMgr.slogan;
-        EXMgr.Bar.command = "luaide-lite.about";
-        EXMgr.Bar.text = EXMgr.extensionName;
+        EXMgr.Bar.tooltip = EXMgr.slogan
+        EXMgr.Bar.command = "luaide-lite.about"
+        EXMgr.Bar.text = EXMgr.extensionName
         EXMgr.Bar.show();
-        EXMgr.requireFunNames = new Array();
-        EXMgr.requireFunNames.push("require");
-        EXMgr.requireFunNames.push("import");
+
+        EXMgr.requireFunNames = new Array<string>();
+        EXMgr.requireFunNames.push("require")
+        EXMgr.requireFunNames.push("import")
         try {
-            EXMgr.extensionPath = vscode.extensions.getExtension(EXMgr.extensionID).extensionPath;
+            EXMgr.extensionPath = vscode.extensions.getExtension(EXMgr.extensionID).extensionPath
             EXMgr.parseConfig();
             EXMgr.isInit = true;
-        }
-        catch (err) {
+        } catch (err) {
             vscode.window.showErrorMessage(EXMgr.extensionName + " init error: " + err);
-            console.log(err);
+            console.log(err)
         }
     }
-    static parseConfig() {
-        var config = vscode.workspace.getConfiguration("luaide-lite");
-        EXMgr.luaOperatorCheck = config.get("luaOperatorCheck");
-        EXMgr.luaFunArgCheck = config.get("luaFunArgCheck");
-        EXMgr.changeTextCheck = config.get("changeTextCheck");
-        EXMgr.moduleFunNestingCheck = config.get("moduleFunNestingCheck");
-        EXMgr.enableHighlight = config.get("enableHighlight");
-        EXMgr.normalHighlightColor = config.get("highlightNormal");
-        EXMgr.darkHighlightColor = config.get("highlightDark");
-        EXMgr.enableFormat = config.get("enableFormat");
-        EXMgr.core = config.get("core");
-        EXMgr.javahome = config.get("javahome");
-        EXMgr.lightParameter = config.get("theme.light.parameter");
-        EXMgr.lightGlobal = config.get("theme.light.global");
-        EXMgr.lightAnnotation = config.get("theme.light.annotation");
-        EXMgr.darkParameter = config.get("theme.dark.parameter");
-        EXMgr.darkGlobal = config.get("theme.dark.global");
-        EXMgr.darkAnnotation = config.get("theme.dark.annotation");
+
+    public static parseConfig() {
+        var config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("luaide-lite")
+        EXMgr.luaOperatorCheck = config.get<boolean>("luaOperatorCheck")
+        EXMgr.luaFunArgCheck = config.get<boolean>("luaFunArgCheck")
+        EXMgr.changeTextCheck = config.get<boolean>("changeTextCheck")
+        EXMgr.moduleFunNestingCheck = config.get<boolean>("moduleFunNestingCheck")
+        EXMgr.enableHighlight = config.get<boolean>("enableHighlight")
+        EXMgr.normalHighlightColor = config.get<string>("highlightNormal")
+        EXMgr.darkHighlightColor = config.get<string>("highlightDark")
+        EXMgr.enableFormat = config.get<boolean>("enableFormat")
+        EXMgr.core = config.get<string>("core")
+        EXMgr.javahome = config.get<string>("javahome")
+        EXMgr.lightParameter = config.get<string>("theme.light.parameter")
+        EXMgr.lightGlobal = config.get<string>("theme.light.global")
+        EXMgr.lightAnnotation = config.get<string>("theme.light.annotation")
+        EXMgr.darkParameter = config.get<string>("theme.dark.parameter")
+        EXMgr.darkGlobal = config.get<string>("theme.dark.global")
+        EXMgr.darkAnnotation = config.get<string>("theme.dark.annotation")
         // single script root.
         var scriptRoot = vscode.workspace.rootPath.replace(/\\/g, "/");
-        scriptRoot = scriptRoot.replace(new RegExp("/", "gm"), ".");
+        scriptRoot = scriptRoot.replace(new RegExp("/", "gm"), ".")
         scriptRoot = scriptRoot.toLowerCase();
-        EXMgr.scriptRoots = new Array();
+        EXMgr.scriptRoots = new Array<string>();
         EXMgr.scriptRoots.push(scriptRoot);
+
         if (EXMgr.luaOperatorCheck == null) {
             EXMgr.luaOperatorCheck = true;
         }
         if (EXMgr.luaFunArgCheck == null) {
-            EXMgr.luaFunArgCheck = true;
+            EXMgr.luaFunArgCheck = true
         }
         if (EXMgr.changeTextCheck == null) {
-            EXMgr.changeTextCheck = true;
+            EXMgr.changeTextCheck = true
         }
         if (EXMgr.enableHighlight == null) {
-            EXMgr.enableHighlight = true;
+            EXMgr.enableHighlight = true
         }
         if (EXMgr.normalHighlightColor == null) {
-            EXMgr.normalHighlightColor = "#2B91AF";
+            EXMgr.normalHighlightColor = "#2B91AF"
         }
         if (EXMgr.darkHighlightColor == null) {
-            EXMgr.darkHighlightColor = "#00D6AA";
+            EXMgr.darkHighlightColor = "#00D6AA"
         }
         if (EXMgr.enableFormat == null) {
-            EXMgr.enableFormat = true;
+            EXMgr.enableFormat = true
         }
         if (EXMgr.core == null) {
-            EXMgr.core = "emmy";
+            EXMgr.core = "emmy"
         }
         if (EXMgr.core == "emmy") {
-            EXMgr.isLegacy = EXMgr.getJavaExe() == null ? true : false;
-        }
-        else {
-            EXMgr.isLegacy = true;
+            EXMgr.isLegacy = EXMgr.getJavaExe() == null ? true : false
+        } else {
+            EXMgr.isLegacy = true
         }
         if (EXMgr.lightParameter == null) {
-            EXMgr.lightParameter = "#565656";
+            EXMgr.lightParameter = "#565656"
         }
         if (EXMgr.lightGlobal == null) {
-            EXMgr.lightGlobal = "#2B91AF";
+            EXMgr.lightGlobal = "#2B91AF"
         }
         if (EXMgr.lightAnnotation == null) {
-            EXMgr.lightAnnotation = "#2B91AF";
+            EXMgr.lightAnnotation = "#2B91AF"
         }
         if (EXMgr.darkParameter == null) {
-            EXMgr.darkParameter = "#FFFFFF";
+            EXMgr.darkParameter = "#FFFFFF"
         }
         if (EXMgr.darkGlobal == null) {
-            EXMgr.darkGlobal = "#00D6AA";
+            EXMgr.darkGlobal = "#00D6AA"
         }
         if (EXMgr.darkAnnotation == null) {
-            EXMgr.darkAnnotation = "#00D6AA";
+            EXMgr.darkAnnotation = "#00D6AA"
         }
-        EXMgr.templateDir = config.get("templateDir");
+
+        EXMgr.templateDir = config.get<string>("templateDir")
         if (EXMgr.templateDir == null || EXMgr.templateDir == "") {
-            EXMgr.templateDir = EXMgr.extensionPath + "/res/template";
+            EXMgr.templateDir = EXMgr.extensionPath + "/res/template"
         }
-        EXMgr.templateDefine = new Map();
-        var templateDefine = config.get("templateDefine");
+        EXMgr.templateDefine = new Map<string, string>()
+        var templateDefine: Array<any> = config.get<Array<any>>("templateDefine")
         if (templateDefine) {
             templateDefine.forEach((v) => {
                 if ((v.name || v.key) && v.value) {
                     if (v.name) {
-                        EXMgr.templateDefine.set(v.name, v.value);
-                    }
-                    else {
-                        EXMgr.templateDefine.set(v.key, v.value);
+                        EXMgr.templateDefine.set(v.name, v.value)
+                    } else {
+                        EXMgr.templateDefine.set(v.key, v.value)
                     }
                 }
-            });
+            })
         }
-        EXMgr.excludes = new Array();
-        var excludes = config.get("exclude");
+
+        EXMgr.excludes = new Array<string>()
+        var excludes: Array<string> = config.get<Array<string>>("exclude");
         if (excludes) {
             excludes.forEach((v) => {
-                EXMgr.excludes.push(path.normalize(path.join(vscode.workspace.rootPath, v)));
-            });
+                EXMgr.excludes.push(path.normalize(path.join(vscode.workspace.rootPath, v)))
+            })
         }
-        EXMgr.excludesRegex = new Array();
-        var excludesRegexs = config.get("excludesRegex");
+        EXMgr.excludesRegex = new Array<string>()
+        var excludesRegexs: Array<string> = config.get<Array<string>>("excludesRegex");
         if (excludesRegexs) {
             excludesRegexs.forEach((v) => {
                 EXMgr.excludesRegex.push(v);
-            });
+            })
         }
-        EXMgr.formatHex = config.get("formatHex");
+
+
+        EXMgr.formatHex = config.get<boolean>("formatHex")
         if (EXMgr.formatHex == null) {
-            EXMgr.formatHex = true;
+            EXMgr.formatHex = true
         }
-        EXMgr.enableDiagnostic = config.get("enableDiagnostic");
+
+        EXMgr.enableDiagnostic = config.get<boolean>("enableDiagnostic")
         if (EXMgr.enableDiagnostic == null) {
-            EXMgr.enableDiagnostic = true;
+            EXMgr.enableDiagnostic = true
         }
-        EXMgr.typescriptDefine = new Map();
-        var typescriptDefine = config.get("typescriptDefine");
+
+        EXMgr.typescriptDefine = new Map<string, string>()
+        var typescriptDefine: Array<any> = config.get<Array<any>>("typescriptDefine")
         if (typescriptDefine) {
             typescriptDefine.forEach((v) => {
                 if ((v.name || v.key) && v.value) {
                     if (v.name) {
-                        EXMgr.typescriptDefine.set(v.name, v.value);
-                    }
-                    else {
-                        EXMgr.typescriptDefine.set(v.key, v.value);
+                        EXMgr.typescriptDefine.set(v.name, v.value)
+                    } else {
+                        EXMgr.typescriptDefine.set(v.key, v.value)
                     }
                 }
-            });
+            })
         }
-        HighlightG_1.HighlightG.reset();
+
+        HighlightG.reset()
     }
-    static isFileExclude(file) {
+
+    public static isFileExclude(file: string): boolean {
         if (EXMgr.excludes && EXMgr.excludes.length > 0) {
             for (var i = 0; i < EXMgr.excludes.length; i++) {
-                var exclude = EXMgr.excludes[i];
+                var exclude = EXMgr.excludes[i]
                 if (file.indexOf(exclude) > -1) {
-                    return true;
+                    return true
                 }
             }
-            return false;
+            return false
         }
         else if (EXMgr.excludesRegex && EXMgr.excludesRegex.length > 0) {
             for (var i = 0; i < EXMgr.excludesRegex.length; i++) {
-                var exclude = EXMgr.excludesRegex[i];
+                var exclude = EXMgr.excludesRegex[i]
                 if (file.search(exclude) > -1) {
-                    return true;
+                    return true
                 }
             }
-            return false;
+            return false
         }
         else {
-            return false;
+            return false
         }
     }
-    static getJavaExe() {
+
+    public static getJavaExe(): string {
         try {
             if (process.platform == "win32") {
                 if (EXMgr.javahome != null) {
-                    return path.join(EXMgr.javahome, "bin/java.exe");
+                    return path.join(EXMgr.javahome, "bin/java.exe")
                 }
                 if ("JAVA_HOME" in process.env) {
-                    let javaHome = process.env.JAVA_HOME;
-                    let javaPath = path.join(javaHome, "bin/java.exe");
-                    return javaPath;
+                    let javaHome = <string>process.env.JAVA_HOME
+                    let javaPath = path.join(javaHome, "bin/java.exe")
+                    return javaPath
                 }
                 if ("PATH" in process.env) {
-                    let PATH = process.env.PATH;
-                    let paths = PATH.split(";");
-                    let pathCount = paths.length;
+                    let PATH = <string>process.env.PATH
+                    let paths = PATH.split(";")
+                    let pathCount = paths.length
                     for (let i = 0; i < pathCount; i++) {
-                        let javaPath = path.join(paths[i], "bin/java.exe");
+                        let javaPath = path.join(paths[i], "bin/java.exe")
                         if (fs.existsSync(javaPath)) {
-                            return javaPath;
+                            return javaPath
                         }
                     }
                 }
             }
+        } catch{
         }
-        catch (_a) {
-        }
-        return null;
+        return null
     }
 }
-EXMgr.extensionID = "wellshsu.luaide-lite";
-EXMgr.extensionName = "luaide-lite";
-EXMgr.slogan = "Lite and free, but more professional.";
-EXMgr.LUA_MODE = { language: "lua", scheme: "file" };
-EXMgr.LANGUAGE_ID = "lua";
-EXMgr.maxFileSize = 2048;
-EXMgr.changeTextCheck = true;
-EXMgr.isInit = false;
-EXMgr.enableHighlight = true;
-EXMgr.normalHighlightColor = "#2B91AF";
-EXMgr.darkHighlightColor = "#00D6AA";
-EXMgr.enableFormat = true;
-EXMgr.core = "emmy";
-EXMgr.lightParameter = "#565656";
-EXMgr.lightGlobal = "#2B91AF";
-EXMgr.lightAnnotation = "#2B91AF";
-EXMgr.darkParameter = "#FFFFFF";
-EXMgr.darkGlobal = "#00D6AA";
-EXMgr.darkAnnotation = "#00D6AA";
-EXMgr.isLegacy = false;
-EXMgr.Commands = [
-    { label: "luaide-lite.toUpperCase", desc: 'Shift chars to uppercase', func: UpperLower_1.UpperLower.toUpperCase },
-    { label: "luaide-lite.toLowerCase", desc: 'Shift carrs to lowercase', func: UpperLower_1.UpperLower.toLowerCase },
-    { label: "luaide-lite.formatFiles", desc: 'Format file(s)', func: FileFormat_1.FileFormat.process },
-    { label: "luaide-lite.createTemplate", desc: 'Create template', func: PvdTemplate_1.PvdTemplate.process },
-    { label: "luaide-lite.convertTsFile", desc: 'Translate lua to typescript', func: PvdLuats_1.PvdLuats.processFile },
-    { label: "luaide-lite.convertTsText", desc: 'Convert lua to typescript', func: PvdLuats_1.PvdLuats.processText },
-    { label: "luaide-lite.openRes", desc: 'Open res folder', func: EXMgr.openRes },
-    { label: "luaide-lite.about", desc: 'About luaide-lite', func: EXMgr.showAbout },
-];
-exports.EXMgr = EXMgr;
-//# sourceMappingURL=EXMgr.js.map
